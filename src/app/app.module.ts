9f415ec8ef3cb9354b8a57328a73a250cfd480e1
Freddy Botteri <>
2022-10-09 17:44:23 +0200

    hexagonal called api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { BudgetApiRestService } from './infraestructure/driven-adapter/apiRest';
+import { BudgetApiRestService } from './infrastructure/driven-adapter/apiRest';
 import { BudgetGateway } from './domain/models/Budget/gateway/budget.gateway';
 import { BudgetComponent } from './views/budget/budget.component';
 import { HttpClientModule } from '@angular/common/http';
